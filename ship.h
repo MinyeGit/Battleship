14c2508f16b718c2fd44c56e55cc0c1be20da729
minye.wu <minye.wu.cs@gmail.com>
2019-09-15 15:34:37 -0500

    update the ship class header file and implementation

--- ship.h
+++ ship.h
@@ -1,16 +1,25 @@
 #ifndef SHIP_H
 #define SHIP_H
-#include <iostream>
 
-class Ship //not sure yet of implementation, so nothing in here yet.
+#include "Cell.h"
+#include <vector>
+
+using namespace std;
+class Ship
 {
   private:
-
+    int orient; //0: xAxis, 1:yAxis
+    int size;
 
   public:
-
+    vector<Cell> cells;
     Ship();
-
+    Ship(int orientation, int size);
+    void setOrient(int orientation);
+    int getOrient();
+    void setSize(int size);
+    int getSize();
+    bool isAlive();
 };
 
 #endif
